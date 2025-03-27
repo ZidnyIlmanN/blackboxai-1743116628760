af99216be06a86b2b10337c8b7fcb0d76b0a1d78
Zidny Ilman Nafi'an <136836161+ZidnyIlmanN@users.noreply.github.com>
2025-03-28 06:04:21 +0700

    Add musik_app/lib/core/constants/app_constants.dart 1743116662113

---
+++ musik_app/lib/core/constants/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String appName = 'Musik App';
+  static const List<String> bottomNavLabels = ['Home', 'Add Music', 'Profile'];
+  static const List<IconData> bottomNavIcons = [
+    Icons.home,
+    Icons.add_circle_outline,
+    Icons.person
+  ];
+}
